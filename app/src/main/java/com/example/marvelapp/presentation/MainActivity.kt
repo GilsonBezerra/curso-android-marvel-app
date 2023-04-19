76836036ff220044fa74dc98882a47b470dee40d
Gilson Bezerra <gilson.bezerra@live.com>
2023-04-18 23:29:44 -0300

    Configura o detekt no projeto

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.marvelapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
